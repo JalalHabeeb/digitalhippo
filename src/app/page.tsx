efefc3bd661622eb84d1c5605c15d782c9ae5fce
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-02-12 09:18:10 +0100

    add buttons using shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,6 +10,16 @@ export default function Home() {
           Your marketplace for high-quality{" "}
           <span className="text-blue-600">digital assets</span>.
         </h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          Welcome to DigitalHippo. Every asset on out platform is verified by
+          our team to ensure our highest quality standards.
+        </p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href="/products" className={buttonVariants()}>
+            Browse Trending
+          </Link>
+          <Button variant="ghost">Our quality promise &rarr;</Button>
+        </div>
       </div>
     </MaxWidthWrapper>
   );
