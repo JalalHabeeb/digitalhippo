269adaeb667ac0fc74bf04c3eedc04c73668cf67
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-02-13 14:03:39 +0100

    add shopping cart from shadcn-ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
 import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 const Navbar = () => {
   const user = null;
