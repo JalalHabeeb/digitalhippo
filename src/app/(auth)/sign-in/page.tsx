ab5f362547093c8b005bd6f2368dd657c045d8ab
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-02-17 21:12:10 +0100

    add sign in code

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -60,15 +60,15 @@ const Page = () => {
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col items-center space-y-2 text-center">
             <Icons.logo className="h-20 w-20" />
-            <h1 className="text-wxl font-bold">Create an account</h1>
+            <h1 className="text-wxl font-bold">Sign in to your account</h1>
             <Link
               className={buttonVariants({
                 variant: "link",
                 className: "gap-1.5",
               })}
-              href="/sign-in"
+              href="/sign-up"
             >
-              Already have an account? Sign-in
+              Don&apos;nt have an account? Sign up
               <ArrowRight className="h-4 w-4" />
             </Link>
           </div>
@@ -108,9 +108,23 @@ const Page = () => {
                   )}
                 </div>
 
-                <Button>Sign up</Button>
+                <Button>Sign in</Button>
               </div>
             </form>
+
+            <div className="relative">
+              <div
+                aria-hidden="true"
+                className="absolute inset-0 flex items-center"
+              >
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  or
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
