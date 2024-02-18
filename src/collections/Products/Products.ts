8a6309505bf9b6a9b64605c9b261a67cb8ab04da
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-02-18 16:58:15 +0100

    add Products collection

---
+++ src/collections/Products/Products.ts
@@ -0,0 +1,40 @@
+import { CollectionConfig } from "payload/types";
+
+export const Products: CollectionConfig = {
+  slug: "products",
+  admin: {
+    useAsTitle: "name",
+  },
+  access: {},
+  fields: [
+    {
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
+      required: true,
+      hasMany: false,
+      admin: {
+        condition: () => false,
+      },
+    },
+    {
+      name: "name",
+      label: "Name",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "description",
+      type: "textarea",
+      label: "product details",
+    },
+    {
+      name: "price",
+      label: "price in USD",
+      min: 0,
+      max: 1000,
+      type: "number",
+      required: true,
+    },
+  ],
+};
