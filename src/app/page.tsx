c31cefa76c6883b7bd969d62dca4cc5b25cef4f5
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-02-21 22:53:38 +0100

    adding product title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
           </div>
         </div>
 
-        <ProductReel />
+        <ProductReel title="Brand new" />
       </MaxWidthWrapper>
 
       <section className="border-t border-gray-200 bg-gray-50">
