a50b0119a78ff68dc787c7fd5e4acf3b230c109b
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-02-25 19:49:11 +0100

    add right arrow to slider

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -2,9 +2,11 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type SwiperType from "swiper";
 import { Pagination } from "swiper/modules";
+import { cn } from "@/lib/utils";
+import { ChevronRight } from "lucide-react";
 
 interface ImageSliderProps {
   urls: string[];
@@ -19,6 +21,16 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
     isEnd: activeIndex === (urls.length ?? 0) - 1,
   });
 
+  useEffect(() => {
+    swiper?.on("slideChange", ({ activeIndex }) => {
+      setActiveIndex(activeIndex);
+      setSlideConfig({
+        isBeginning: activeIndex === 0,
+        isEnd: activeIndex === (urls.length ?? 0) - 1,
+      });
+    });
+  }, [swiper, urls]);
+
   const activeStyles =
     "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
 
@@ -27,7 +39,20 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
   return (
     <div className="group relative bg-zinc-100 aspect-square overflow-hidden rounded-xl">
       <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
-        <button></button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slideNext();
+          }}
+          className={cn(activeStyles, "right-3 transition", {
+            [inActiveStyles]: slideConfig.isEnd,
+            "hover: bg-primary-300 text-primary-800 opacity-100":
+              !slideConfig.isEnd,
+          })}
+          aria-label="next image"
+        >
+          <ChevronRight className="h-4 w-4 text-zinc-700" />
+        </button>
         <button></button>
       </div>
       <Swiper
