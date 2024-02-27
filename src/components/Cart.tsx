e08a5f809e83aa36e03faf0d59e4cd9f56b516ae
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-02-27 20:42:05 +0100

    remove item from cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -39,12 +39,12 @@ const Cart = () => {
       >
         <ShoppingCartIcon className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-          0
+          {itemCount}
         </span>
       </SheetTrigger>
       <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
         <SheetHeader className="space-y-2.5 pr-6">
-          <SheetTitle>Cart(0)</SheetTitle>
+          <SheetTitle>Cart ({itemCount})</SheetTitle>
         </SheetHeader>
 
         {itemCount > 0 ? (
@@ -52,7 +52,7 @@ const Cart = () => {
             <div className="flex w-full flex-col pr-6">
               <ScrollArea>
                 {items.map(({ product }) => (
-                  <CartItem key={product.id} />
+                  <CartItem key={product.id} product={product} />
                 ))}
               </ScrollArea>
             </div>
