8c0160eb2d6f38dec326aed1752531a9f6fffb24
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-03-04 10:08:06 +0100

    add stripe for the payment and add transaction fee product

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 import { privateProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { getPayloadClient } from "../get-payload";
+import { stripe } from "@/lib/stripe";
+import type Stripe from "stripe";
 
 export const paymentRouter = router({
   createSession: privateProcedure
@@ -28,5 +30,39 @@ export const paymentRouter = router({
           },
         },
       });
+
+      const filteredProducts = products.filter((prod) => Boolean(prod.priceId));
+
+      const order = await payload.create({
+        collection: "orders",
+        data: {
+          _isPaid: false,
+          products: filteredProducts,
+          user: user.id,
+        },
+      });
+
+      const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+      line_items.push({
+        price: "price_1OqXTOA6JbFQU0yZbbmUroPp",
+        quantity: 1,
+        adjustable_quantity: {
+          enabled: false,
+        },
+      });
+
+      try {
+        const stripeSession = await stripe.checkout.sessions.create({
+          success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/thank-you?orderId=${order.id}`,
+          cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/cart`,
+          payment_method_types: ["card", "paypal"],
+          metadata: {
+            userId: user.id,
+            orderId: order.id,
+          },
+          line_items,
+        });
+      } catch (error) {}
     }),
 });
