d02cdc7e7826a2cb7b07f8fd2b638d9aa9c0cfd4
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-03-07 17:12:00 +0100

    user check

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,10 +1,48 @@
+import { getServerSideUser } from "@/lib/payload-utils";
 import Image from "next/image";
-import React from "react";
+import { cookies } from "next/headers";
+import { getPayloadClient } from "@/get-payload";
+import { notFound, redirect } from "next/navigation";
 
-const ThanksYouPage = () => {
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const ThanksYouPage = async ({ searchParams }: PageProps) => {
+  const orderId = searchParams.orderId;
+
+  const nextCookies = cookies();
+
+  const { user } = await getServerSideUser(nextCookies);
+
+  const payload = await getPayloadClient();
+
+  const { docs: orders } = await payload.find({
+    collection: "orders",
+    depth: 2,
+    where: {
+      id: {
+        equals: orderId,
+      },
+    },
+  });
+
+  const [order] = orders;
+
+  if (!order) return notFound();
+
+  const orderUserId =
+    typeof order.user === "string" ? order.user : order.user.id;
+
+  if (orderUserId !== user?.id) {
+    return redirect(`/sign-in?origin=thank-you?orderId=${order.id}`);
+  }
+
   return (
     <main className="relative lg:min-h-full">
-      <div className="hidden md:block h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12">
+      <div className="hidden lg:block h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12">
         <Image
           fill
           src="/checkout-thank-you.jpg"
